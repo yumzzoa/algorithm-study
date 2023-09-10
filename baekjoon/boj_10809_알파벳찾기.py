aa51b340605132151da3f6d89446b5315e8fb4e7
Yumi Kim <136227196+yumzzoa@users.noreply.github.com>
2023-09-10 19:58:13 +0900

    구현, 문자열, find함수 사용

---
+++ baekjoon/boj_10809_알파벳찾기.py
@@ -0,0 +1,8 @@
+#10809
+
+S = input()
+
+A = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in range(len(A)):
+    print(S.find(A[i]), end=" ")
