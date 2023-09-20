0013d92273009722d7666dd303bb6e5ed7b80a33
Yumi Kim <136227196+yumzzoa@users.noreply.github.com>
2023-09-20 21:34:10 +0900

    문자열, if문 continue, break

---
+++ baekjoon/boj_1316_그룹 단어 체커.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = n
+
+for i in range(n):
+    word = input()
+    for j in range(len(word)-1):
+        if word[j] == word[j+1]:
+            pass
+        elif word[j] in word[j+1:]:
+            cnt = cnt - 1
+            break
+print(cnt)
